cbcd891e74ce1d4f65e835f6af05dc5f7bc1baed
4r9k d0rk <45066935+4R9KD0RK@users.noreply.github.com>
2019-02-17 23:41:28 -0800

    Add files via upload
    Hii  this is python Script for Website Admin Finder 1337

---
+++ trn.py
@@ -0,0 +1,22 @@
+#Coded by The N45UR N4X0R
+import string,os
+os.system("clera")
+def banner():
+	  print "\t##############################################"
+	  print "\t##############################################"
+    def translator(a):
+    	text = a
+    	lower=
+    string.maketrans("abeiost","4831057")
+        upper=
+    string.maketrans("ABEIOST","4931057")
+        t_text = text.translate(lower)
+        t_text = text.translate(upper)
+        print "\n\n\rTranslated text: %s"
+    % (t_text)
+    banner()
+    a = raw_input("Enter Text")
+    translator(a)
+
+
+    	
